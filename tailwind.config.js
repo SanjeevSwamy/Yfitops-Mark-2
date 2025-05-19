a022078d20bd15bd529ff39b1a663d585bf353d2
Sanjeev Swamy <96098664+SanjeevSwamy@users.noreply.github.com>
2025-05-19 21:59:35 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  darkMode: 'class', // <--- This enables class-based dark mode
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        accent: '#1DB954', // Spotify green
+      },
+    },
   },
   plugins: [],
 };
